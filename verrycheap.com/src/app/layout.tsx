a77628d2a237a696cf65291f549d75da048030c7
Yassr atti <cloudec31@gmail.com>
2025-10-04 18:34:16 +0200

    n

--- verrycheap.com/src/app/layout.tsx
+++ verrycheap.com/src/app/layout.tsx
@@ -32,6 +32,10 @@ export default function RootLayout({
     <html lang="en">
       <head>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        {/* <script
+          crossOrigin="anonymous"
+          src="//unpkg.com/react-scan/dist/auto.global.js"
+        /> */}
       </head>
 
       <body
