ebd38491ec947a4e1101ffea2443511a6e9d0aae
Yassr atti <cloudec31@gmail.com>
2025-10-05 16:23:46 +0200

    gradiente in the carousel and what we do section

--- verrycheap.com/src/components/_components/unknown.tsx
+++ verrycheap.com/src/components/_components/unknown.tsx
@@ -1,3 +1,9 @@
 export default function Unknows() {
-  return <div className="h-screen w-full border"></div>;
+  return (
+    <div className="h-screen w-full ">
+      <div className="w-full mt-10 flex items-center justify-center ">
+        <h1 className="text-blue-800 font-semibold text-base">What we do?</h1>
+      </div>
+    </div>
+  );
 }
