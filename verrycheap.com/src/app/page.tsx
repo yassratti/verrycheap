8acbdb938a1e0b02f9a7ff5e576b348e7a9e0126
Yassr atti <cloudec31@gmail.com>
2025-10-07 18:16:00 +0200

    changing to faq section

--- verrycheap.com/src/app/page.tsx
+++ verrycheap.com/src/app/page.tsx
@@ -3,7 +3,7 @@ import { Header } from "@/components/_components/header";
 import Hero from "@/components/_components/hero";
 import Whatwedo from "@/components/_components/what-we-do";
 import Carousel from "@/components/_components/carousel";
-import Unknows from "@/components/_components/unknow";
+import Unknows from "@/components/_components/faq";
 export default function Home() {
   return (
     <div className="w-screen h-screen">
