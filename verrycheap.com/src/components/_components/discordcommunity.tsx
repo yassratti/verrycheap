40eba14fa65986973189280a6c896cb6e906efeb
Yassr atti <cloudec31@gmail.com>
2025-10-10 16:29:15 +0200

    discord community copy

--- verrycheap.com/src/components/_components/discordcommunity.tsx
+++ verrycheap.com/src/components/_components/discordcommunity.tsx
@@ -9,8 +9,8 @@ export function WobbleCardDemo() {
       <WobbleCard containerClassName="col-span-1 lg:col-span-3 bg-blue-900 min-h-[450px] lg:min-h-[600px] xl:min-h-[300px]">
         <div className="max-w-sm">
           <h2 className="max-w-sm md:max-w-lg  text-left text-balance text-base md:text-xl lg:text-3xl font-semibold tracking-[-0.015em] text-white">
-            Signup for blazing-fast cutting-edge state of the art Gippity AI
-            wrapper today!
+            If you want to contact the creator or request a plan that&apos;s not
+            listed here, join our Discord community!
           </h2>
           {/* <p className="mt-4 max-w-[26rem] text-left  text-base/6 text-neutral-200">
             With over 100,000 mothly active bot users, Gippity AI is the most
