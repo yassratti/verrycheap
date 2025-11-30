788703b938044b116283a8f24a03cd14a6bcd480
Yassr Atti <cloudec31@gmail.com>
2025-12-01 00:09:32 +0100

    feat(first steps): adding a button without functionality to test prod

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,12 @@
 "use client";
 
+import Header from "@/components/_components/header";
 export default function Home() {
-  return <div className="w-screen h-screen">{/* pivot */}</div>;
+  return (
+    <div className="w-screen h-screen">
+      <Header />
+
+      {/* pivot */}
+    </div>
+  );
 }
