6bb0e9a5c22aaed7bd581d975abeceb5e43a1259
Yassr Atti <cloudec31@gmail.com>
2025-12-10 13:13:17 +0100

    feat: adding the needed componentes to management

--- src/components/sections/management-section.tsx
+++ src/components/sections/management-section.tsx
@@ -5,6 +5,11 @@ import { SidebarTrigger } from "@/components/ui/sidebar";
 import { Button } from "@/components/ui/button";
 import { Save } from "lucide-react";
 
+import { SectionCards } from "@/components/_components/_components/section-card";
+import { ChartAreaInteractive } from "../_components/_components/chart-area-interactive";
+import { DataTable } from "../_components/_components/data-table";
+
+import data from "../_components/_components/data.json";
 export function ManagementSection() {
   return (
     <>
@@ -32,6 +37,17 @@ export function ManagementSection() {
       </header>
 
       {/* Contenido de la sección management */}
+      <div className="flex flex-1 flex-col">
+        <div className="@container/main flex flex-1 flex-col gap-2">
+          <div className="flex flex-col gap-4 py-4 md:gap-6 md:py-6">
+            <SectionCards />
+            <div className="px-4 lg:px-6">
+              <ChartAreaInteractive />
+            </div>
+            <DataTable data={data} />
+          </div>
+        </div>
+      </div>
     </>
   );
 }
